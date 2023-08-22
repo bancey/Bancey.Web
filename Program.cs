e0ec61b27d5785a62ffa85a50952db5e155f6378
Alex Bance <abance@bancey.xyz>
2023-08-22 17:43:32 +0100

    feat: update to .NET 7

--- Program.cs
+++ Program.cs
@@ -1,15 +1,5 @@
-﻿using System.Threading.Tasks;
-using Statiq.App;
-using Statiq.Web;
-
-namespace Bancey.Web
-{
-    public class Program
-    {
-        public static async Task<int> Main(string[] args) =>
-            await Bootstrapper
-              .Factory
-              .CreateWeb(args)
-              .RunAsync();
-    }
-}
+﻿return await Bootstrapper
+  .Factory
+  .CreateWeb(args)
+  .RunAsync();
+  
